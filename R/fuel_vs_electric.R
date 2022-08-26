eada614c82c75de9fb4acdcf82871c6b64af1a7f
JaviPatron <jpatronmf@gmail.com>
2022-08-26 08:54:21 -0700

    explaining the roxygen skeleton

--- R/fuel_vs_electric.R
+++ R/fuel_vs_electric.R
@@ -3,19 +3,37 @@
 #' In this Markdown document we will create a model on rental cars prices so you can select the best option regarding price and carbon offset
 #'
 #' @param tot_days (Total Days of your trip)
-#' @param electric_rent_cost
-#' @param dist_plan
-#' @param full_battery_cost
-#' @param range_electric
-#' @param fuel_rent_cost
-#' @param full_tank_cost
-#' @param range_fuel
+#' @param electric_rent_cost (Electric rental car rate, per day)
+#' @param dist_plan (Estimated distance covered in your trip)
+#' @param full_battery_cost (Cost to fully charge your electric rental car)
+#' @param range_electric (Electric car range when its fully charged)
+#' @param fuel_rent_cost (Fuel rental car rate, per day)
+#' @param full_tank_cost (Cost to fill up the tank)
+#' @param range_fuel (Fuel vehicle range distance (miles))
 #'
 #' @return
 #' @export
 #'
 #' @examples
-price_delta <- function(tot_days, electric_rent_cost, dist_plan, full_battery_cost, range_electric, fuel_rent_cost, full_tank_cost, range_fuel) {
+#'
+#' Decision making at a Hertz Rental Car agency at the LA Airport.
+#' The trip is just for the weekend = 3 days
+#' There will be some addional trips around, but the estimated total distance is
+#' LA to SB = 105.5 Miles
+#' Weekend trips = + 100 miles
+#' Total planned distance = 105.5 + 100 + 105.5 = 311 miles
+#'
+#'
+#'Add some default configurations.
+#'Add example
+#'
+#'Pass the results with return
+#'
+#'
+#' price_delta(3,100,500,18.29,405,40,51.50,446)
+#'
+#'
+price_delta <- function(tot_days, electric_rent_cost = 100, dist_plan, full_battery_cost, range_electric, fuel_rent_cost, full_tank_cost, range_fuel) {
 
   electric_cost <- ((tot_days * electric_rent_cost) + (dist_plan * full_battery_cost)/range_electric)
 
@@ -37,14 +55,14 @@ B <- c(electric_rent_cost, fuel_rent_cost)
 intersection <- solve(A,B)
 
 
-print(paste0("The point of intersection for both cars is at $", round(intersection[1], 2), " miles, where the price is $", round(intersection[2],2), "  for both options"))
+print(paste0("The point of intersection for both cars is at ", round(intersection[1], 2), " miles, where the price is $", round(intersection[2],2), "  for both options"))
 
 
 ggplot(data=data.frame( x=c(0,2000),y=c(0,500) ), aes(x=x,y=y)) +
   geom_point(shape = 2) +
   geom_abline(intercept = electric_rent_cost , slope = slope_electric, col = "red") +
   geom_abline(intercept = fuel_rent_cost , slope = slope_fuel, col = "blue") +
-  labs(title = "Fuel vs Electric car price",
+  labs(title = "Fuel vs Electric Car Price",
        x = "Distance (Miles",
        y = "Total Price (Dls)")+
   geom_hline(yintercept = 0) +
